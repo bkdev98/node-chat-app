2f0e1c2adac1238e14c1d6c8dca96985775b870c
bkdev98 <bkdev98@gmail.com>
2017-01-14 23:51:36 +0700

    Init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.send(publicPath);
+});
+
+app.listen(3000, (err) => {
+  console.log(`Server is up on port ${port}`);
+});
