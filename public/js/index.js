36cc7e024d4225fcd1e397225602325f104a1c50
bkdev98 <bkdev98@gmail.com>
2017-01-15 17:51:29 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'bkdev',
-    text: 'Coffee?'
-  });
 });
 
 socket.on('disconnect', function () {
